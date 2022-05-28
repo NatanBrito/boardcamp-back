2a876e370482f0c0cca32ece2be1da98323f5f7e
Natan Brito <natanismaelbrito@gmail.com>
2022-05-28 01:08:32 -0300

    save

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,5 +1,28 @@
 import db from '../db.js';
 
 export async function getGames(req,res){
-    res.sendStatus(202)
+    const name=req.query.name;
+    
+
+try{
+    const Listgames= await db.query(`
+    SELECT * FROM games
+    `);
+     const games= Listgames.rows
+    if(name){
+      const search=games.filter((game)=>{
+         
+      if((game.name).includes(name)){
+          return game
+      }
+      })  
+      res.send(search);
+      return;
+    }
+     res.send(games).status(200);
+    return;
+    }catch(e){
+        return res.sendStatus(400);
+    }
+    res.sendStatus(201);
 }
--- src/db.js
+++ src/db.js
@@ -1,19 +1,8 @@
 import pg from 'pg';
 import dotenv from "dotenv";
-// .ENV  nao funcionando
-// dotenv.config();
+dotenv.config();
 const {Pool}= pg;
-const user='postgres';
-const password='19951995';
-const host='localhost';
-const port=5432;
-const database='boardcamp';
-
 const db = new Pool({
-user,
-password,
-host,
-port,
-database
+    connectionString: process.env.DATABASE_URL,
 });
 export default db;
