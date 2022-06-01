afa3d85992d8fff1e55a1d5dd34c0e9a4b34e04f
Natan Brito <natanismaelbrito@gmail.com>
2022-06-01 08:56:55 -0300

    authorizations

--- src/db.js
+++ src/db.js
@@ -2,6 +2,10 @@ import pg from 'pg';
 import dotenv from "dotenv";
 dotenv.config();
 const {Pool}= pg;
+
+{
+    rejectUnauthorized: false
+}
 const db = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
